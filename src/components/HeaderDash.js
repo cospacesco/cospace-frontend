0a0ff8b29b8a3e4c730b1c87ff0f5a2ec3b9c414
Mohamed Sayyaf <m.sayyaf01@gmail.com>
2020-12-18 23:10:17 +0400

    dashboard boilerplate

--- src/components/HeaderDash.js
+++ src/components/HeaderDash.js
@@ -1,45 +1,63 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
-
-const useStyles = makeStyles(() => ({
-  root: {
-    flexGrow: 1,
-  },
+import palette from "../theme/palette";
+import { Link as RouterLink } from "react-router-dom";
+import clsx from "clsx";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+// import HomeLogo from "../images/homepage.svg";
+const useStyles = makeStyles((theme) => ({
+  // root: {
+  //   flexGrow: 1,
+  // },
   // menuButton: {
   //   marginRight: theme.spacing(2),
   // },
   title: {
     flexGrow: 1,
   },
+  //
+  //
+  // Below topbar.css
+  //
+  root: {
+    boxShadow: "none",
+    backgroundColor: palette.primary.dark,
+  },
+  flexGrow: {
+    flexGrow: 1,
+  },
+  link: {
+    "font-family": "Roboto",
+    display: "flex",
+    alignItems: "center",
+    padding: "10px 15px",
+    textDecoration: "none",
+    color: palette.text.link,
+    borderRadius: "5px",
+    transition: "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+    "&:hover": {
+      textDecoration: "none",
+      color: palette.text.link,
+      backgroundColor: "rgba(213,213,226,.1)",
+    },
+  },
+  signOutButton: {
+    marginLeft: theme.spacing(1),
+  },
+  //
+  // Above Topbar css
+  //
 }));
 
 const HeaderDash = () => {
   const classes = useStyles();
   return (
-    <AppBar {...rest} className={clsx(classes.root, className)}>
+    <AppBar className={classes.root}>
       <Toolbar>
-        <RouterLink to="/">
-          <img alt="Logo" src="/images/logo/logo.svg" />
-        </RouterLink>
         <div className={classes.flexGrow} />
-        <a
-          className={classes.link}
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://github.com/cube-js/cube.js"
-        >
-          <img alt="GitHub" src="/images/github.svg" />
-          <span className={classes.git}>Github</span>
-        </a>
-        <a
-          className={classes.link}
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://slack.cube.dev/"
-        >
-          <img alt="Slack" src="/images/slack.svg" />
-          <span className={classes.git}>Slack</span>
+        <a className={classes.link} rel="noopener noreferrer" href="/signin">
+          <ExitToAppIcon />
         </a>
       </Toolbar>
     </AppBar>
